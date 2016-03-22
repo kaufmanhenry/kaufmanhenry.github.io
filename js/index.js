0a3915239f8b6f35da0dd7dafd835209ee16dbf1
Henry Kaufman <hkaufman19@gmail.com>
2016-03-21 20:33:59 -0600

    Small Updates to my portfolio!

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'html/work.html'
+        })
+        .when('/about', {
+            templateUrl: 'html/about.html'
+        })
+        .when('/lab', {
+            templateUrl: 'html/lab.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
