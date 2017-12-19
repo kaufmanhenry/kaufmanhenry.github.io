e6293e8512be95e6263747ee2c7758e71216e321
Henry Kaufman <hkaufman19@gmail.com>
2017-12-18 20:45:30 -0700

    Setting up the default style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import Head from './Head';
+
+const StyledApp = styled.div`
+  margin: 0 auto;
+  max-width: 640px;
+  padding: 80px 0;
+  width: 90%;
+`;
 
 export default function() {
   return (
-    <div>
+    <StyledApp>
       <Head />
       Hi, my name is Henry.
-    </div>
+    </StyledApp>
   );
 }
