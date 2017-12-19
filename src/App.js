1bbeb426436220ef6e4a23176efc109e0c225e09
Henry Kaufman <hkaufman19@gmail.com>
2017-12-18 20:37:47 -0700

    starting something new! :ox:

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Head from './Head';
+
+export default function() {
+  return (
+    <div>
+      <Head />
+      Hi, my name is Henry.
+    </div>
+  );
+}
---
+++ src/Head.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <React.Fragment>
+    <title>Henry Kaufman</title>
+    <style dangerouslySetInnerHTML={{
+      __html: 'body{font-family:-apple-system,BlinkMacSystemFont,sans-serif}'
+    }} />
+  </React.Fragment>
+);
